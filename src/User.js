80b282b5895e9e14fce20d8533e39994c9ce4ea2
Fikun2022 <115298954+Fikun2022@users.noreply.github.com>
2022-10-26 18:51:39 +0100

    Update User.js

--- src/User.js
+++ src/User.js
@@ -1,5 +1,56 @@
-class User {
+//class User {
     // User code here
+
+    
+// create a scooter customer class 
+class User {
+
+  constructor (userName, passWord, age) {
+
+      this._userName = userName
+      this._passWord = passWord
+      this._age = age
+      this._userId = Math.floor(Math.random() * 1000)
   }
+
+  get userId() {  
+    
+    return this._userId
+   }
+
+  get userName() { 
+    
+    return this._userName 
+  }
+
+  set userName (userName) { 
+    
+    this._userName = userName 
+   }
+
+  get passWord(){ 
+    
+    return this._passWord 
+   }
+
+  set passWord (passWord) { 
+    
+    this._passWord = passWord 
+  }
+
+  get age (){
+    
+    return this._age 
+   }
+
+  set age (age) { 
+    
+    this._age = age
+  
+  }
+
+
+}
+
   
   module.exports = User
