14ff682ccd722fc7e836a13e162fdb4cf0cee1e4
Fikun2022 <115298954+Fikun2022@users.noreply.github.com>
2022-10-26 18:52:32 +0100

    Update ScooterApp.test.js

--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -2,7 +2,31 @@ const Scooter = require('../src/Scooter')
 const User = require('../src/User')
 const ScooterApp = require('../src/ScooterApp')
 
-// ScooterApp tests here
+// create user instance
+
+const User2 = new User ('Sam', 'Pass@word1', 44)
+
+// create scooter instance 
+
+const scooter3 = new Scooter('60mph', '77kg', '225mm', '£190 pounds')
+
+// call ScooterApp static methods and tests here
+
+const scooterApp1 = new ScooterApp('10', '26-10-2022', '08:00am', '17:00pm', User2.userName, scooter3.ScooterId)
+
+describe (' Test scooterApp properties here', () => {
+
+// test scooter hire duration
+
+test ('can get hire duration', () => {
+        expect(scooterApp1.hireDuration).toBe("10")
+    })
+
+
+
+
+})
+
 
 // register user
 
